d35d1f677788a693fd04a7774d56f1c55889a10c
albertg1114 <ajgonzalez1114@gmail.com>
2019-02-09 09:32:31 -0500

    pushed to heroku

--- routes/api/users.js
+++ routes/api/users.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
-const usersController = require("../../controllers/usersController");
 
 // Load input validation
 const validateRegisterInput = require("../../validation/register");
